c5f9c449ef61ad970f47e153c4f26bdc47ea4eb9
Stefano Casasola <ste.casasola@gmail.com>
2026-04-10 13:05:26 +0200

    Finishes the recap

--- recap.js
+++ recap.js
@@ -219,3 +219,96 @@ daiLaMano() // <-- invocazione di funzione, ora la sto ESEGUENDO
 
 // però ogni tanto le funzioni trovano delle limitazioni nel codice che si trovano ad eseguire, più che altro
 // perchè questo codice non può subire variazioni
+
+// la funzione daiLaMano() al momento è in grado solamente di presentarsi a nome Stefano, per utilizzare
+// un altro nominativo o creiamo una seconda funzione (sconsigliato, perchè deve fare le stesse cose ma
+// solamente utilizzando un nome diverso) oppure togliamo il dato, la parola 'Stefano', e lo sostituiamo
+// con un PARAMETRO: un parametro è una sorta di "placeholder", un valore temporaneo che poi verrà rimpiazzato
+// dal dato vero e proprio (definito "argomento") che verrà fornito alla funzione durante la sua INVOCAZIONE.
+
+const ioMiChiamo = (nomeFornito = 'Mario') => {
+  //   alert('Io mi chiamo ' + nomeFornito) // 'Io mi chiamo Antonio' esempio
+  alert(`Io mi chiamo ${nomeFornito}`) // 'Io mi chiamo Antonio' esempio
+}
+
+ioMiChiamo('Stefano')
+ioMiChiamo('Robespierre')
+ioMiChiamo('Romeo')
+
+ioMiChiamo() // 'Io mi chiamo Mario', perchè non avete dato l'argomento per il parametro "nomeFornito"
+
+// VALORI DI RITORNO
+
+// immaginate una funzione che fa un calcolo complesso
+// somma due numeri, li eleva al quadrato, e sottrae 17
+
+const complexMath = function (num1, num2) {
+  const sommo = num1 + num2
+  const elevo = sommo * sommo
+  const sottraggo = elevo - 17
+  const risultatoFinale = sottraggo
+  alert(risultatoFinale)
+}
+
+complexMath(50, 72) // 14867
+
+// l'idea è di SPEZZARE complexMath in diversi step; ognuno di questi step farà un'operazione SINGOLA (somma,
+// elevazione etc.) e restituisce il risultato del suo pezzettino di calcolo
+
+const somma = function (num1, num2) {
+  const risultato = num1 + num2
+  return risultato
+}
+
+const eleva = function (num1) {
+  return num1 * num1
+}
+
+const sottrae = function (num1) {
+  return num1 - 17
+}
+
+sottrae(eleva(somma(50, 72))) // 14867, lo stesso di complexMath() ma ora i pezzi sono distinti e posso anche
+// utilizzarli per operazioni scollegate tra di loro
+
+somma(2, 3) // posso anche usare i pezzi singolarmente!
+
+// DOM MANIPULATION
+// La manipolazione del DOM (document object model) ci permette di manipolare il contenuto della pagina tramite JS.
+// Con JS possiamo creare, eliminare, modificare, applicare stili, CSS, classi, attributi a tutti gli elementi della pagina.
+// tutto parte da un oggettone che si chiama "document"
+
+// tipicamente grazie a document parte la fase 1: la RICERCA degli elementi con cui interagire
+// 1) ATTRAVERSAMENTO DEL DOM ("DOM traversing")
+// per recuperare gli elementi del DOM che ci interessano abbiamo diverse metodologie:
+// a) document.getElementById() che ci permette di selezionare un elemento a partire da un id
+// b) document.getElementsByClassName che ci permette di selezionare più elementi a partire da una classe
+// c) document.getElementsByTagName che ci permette di selezionare più elementi a partire da un nome di tag
+// d) document.querySelector() che ci permette di selezionare un elemento a partire da un selettore CSS
+// e) document.querySelectorAll() che ci permette di selezionare più elementi a partire da un selettore CSS
+
+const link = document.querySelector('nav ul li:nth-of-type(3)') // il terzo link della barra di navigazione
+const section = document.getElementById('hero') // elemento con id="hero"
+
+// 2) MANIPOLAZIONE DEGLI ELEMENTI
+// i riferimenti recuperati dai metodi sopra citati sono degli OGGETTI, con un sacco di proprietà e metodi
+// a vostra disposizione.
+link.innerText // permette di leggere/scrivere il contenuto testuale dell'elemento
+link.innerHTML // permette di leggere/scrivere il contenuto HTML dell'elemento
+link.classList.add('nuova-classe') // aggiunge una classe CSS
+link.classList.remove('nuova-classe') // rimuove una classe CSS
+link.classList.toggle('nuova-classe') // aggiunge se manca, toglie se c'è già una classe CSS
+link.style.color = 'red' // aggiunge uno stile inline
+
+// creare elementi da zero
+const title = document.createElement('h2')
+title.innerText = 'Benvenuto ' + characters[1].name
+
+section.appendChild(title)
+
+section.remove()
+
+title.addEventListener('click', function () {
+  // al click del titolo, ingrandiscilo
+  title.style.transform = 'scale(1.2)'
+})
